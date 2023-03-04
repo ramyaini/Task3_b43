208d09d7c15fb6aa1dee7454e8971ce4e8c7e5e0
ramyaini <124415380+ramyaini@users.noreply.github.com>
2023-03-04 11:27:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let MyResume={
+    "basics": {
+      "Name": "Ramya T",
+      "Email": "ramyarams2729@gamil.com",
+      "Phone": 9944733323,
+      "Degree": "B.E",
+      "Location": {
+        "Address": "16/406C,Aravindar Colony",
+        "PostalCode": 641104,
+        "City": "Coimbatore",
+        "State": "Tamilnadu",
+        "Country": "India"
+      },
+      
+
+    },
+
+    "Current Working": 
+      {
+        "Company": "Aggregate Intelligence",
+        "Position": "Senior Research Analyst",
+        "StartDate": "2020-11-09",
+        "EndDate": "2023-02-02",
+      
+      },
+
+      "Experience": [
+        {
+        "Company": "Mobius 365 media",
+        "Position": "Research Analyst",
+        "StartDate": "2020-11-09",
+        "EndDate": "2020-01-02",
+
+      
+      },
+    ],
+    "Education": [
+      {
+        "Institution": "Kathir College of Engineering",
+        "Department": "ECE",
+    
+        "Batch start year": 2012,
+        "Batch end year": 2016,
+        "CGPA": 8.1,
+      }
+    ],
+    "Skills Set": [
+      {
+        "Name": "SQL,Advance Excel",
+        "Level": "Begginer",
+        
+      }
+    ],
+    "Languages Known": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+
+}
+  console.log(MyResume);
+//---------------------JSON iteration----------  
+  var json = [{
+    "name" : "ramya", 
+    "batch"   : "b43",
+    "age" : "28",
+    "Batchtype": "weekend"
+},
+{
+    "id" : "Indu", 
+    "batch"   : "b42",
+    "age" : "21",
+    "Batchtype": "Weekday"
+}];
